5cf8a9fa7ff737aaf457eb086b959ec8d0f01a43
Carlos Brito <cbrito1994@gmail.com>
2021-08-04 18:40:52 -0500

    Created the pages folder with the routes

---
+++ src/pages/AboutMe.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function AboutMe() {
+    return (
+        <main className="aboutMe">
+            <h1>About Me</h1>
+            <img src="" alt="" />
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo necessitatibus, dolores totam quos amet enim exercitationem eaque in quia fuga eligendi officia nihil omnis, officiis unde ad ea sed ipsum?</p>
+        </main>
+    )
+}
+
+export default AboutMe
