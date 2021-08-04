ec5e3ef2011d72b332255a8ccf4c07cb9c7ec38a
Carlos Brito <cbrito1994@gmail.com>
2021-08-04 18:36:03 -0500

    Created the portfolio component

---
+++ src/components/Portfolio.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Portfolio() {
+    return (
+        <main className="portfolio">
+            <h1>Portfolio</h1>
+            <div className="portfolio__projects">
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <img src="" alt="" />
+            </div>
+        </main>
+    )
+}
+
+export default Portfolio
