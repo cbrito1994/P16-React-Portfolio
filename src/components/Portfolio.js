5571a0f5ac657694be994eae7d134f76c2967802
Carlos Brito <cbrito1994@gmail.com>
2021-08-04 21:07:49 -0500

    Modified the portfolio component

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import PortfolioProjects from './PortfolioProjects'
 
 function Portfolio() {
     return (
         <main className="portfolio">
             <h1>Portfolio</h1>
             <div className="portfolio__projects">
-                <img src="" alt="" />
-                <img src="" alt="" />
-                <img src="" alt="" />
+                <PortfolioProjects photoClass="mainPicture" title="Covid Tracker" techs="HTML / JS / Foundation" link={"https://github.com/gutihi85/covid_tracker"} />
+                <PortfolioProjects photoClass="secondPic" title="Password Generator" techs="HTML / JS / CSS" link={"https://github.com/cbrito1994/H3-Password-Generator"} />
+                <PortfolioProjects photoClass="thirdPic" title="Hockey Quiz" techs="HTML / JS / CSS" link={"https://github.com/cbrito1994/P4-Hockey-Quiz"} />
             </div>
         </main>
     )
