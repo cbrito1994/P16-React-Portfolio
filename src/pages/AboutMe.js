4e5e3d9f5e297b61ac7c873dc97fc5dfb63113b8
Carlos Brito <cbrito1994@gmail.com>
2021-08-04 21:09:04 -0500

    Added some extra info

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import carlosPic from '../assets/Foto-Carlos-Brito.jpeg'
 
 function AboutMe() {
     return (
         <main className="aboutMe">
             <h1>About Me</h1>
-            <img src="" alt="" />
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo necessitatibus, dolores totam quos amet enim exercitationem eaque in quia fuga eligendi officia nihil omnis, officiis unde ad ea sed ipsum?</p>
+            <p style={{ whiteSpace: "pre-line" }}>
+                My name is Carlos Brito, I'm 27 years old and I'm an accountant.
+
+                I live in Mexico City, my hobbies are ice skating, watching movies, going out with friends and always learning new stuff.
+                
+                The reason I started coding is because I like to create things and in coding I found the best way to do so while having a lot of fun.
+            </p>
+            <img src={carlosPic} alt="mypicture" />
         </main>
     )
 }
